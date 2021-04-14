99a7461b1a9b3ec4b15a7a512d187378b1cb3f9b
Muzammil Rafiq <57823963+Muzammil-crypto@users.noreply.github.com>
2021-04-14 15:52:28 +0500

    adressBook

---
+++ lib/Model/user.dart
@@ -0,0 +1,11 @@
+class UserModel {
+  String _email;
+  String _password;
+  String get email => this._email;
+
+  set email(String value) => this._email = value;
+
+  get password => this._password;
+
+  set password(value) => this._password = value;
+}
